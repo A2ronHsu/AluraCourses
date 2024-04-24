e4a93aa6c23c8a017e725ab1129595a0ddd92add
Aaron <aaronbee1990@gmail.com>
2024-04-24 13:16:53 -0400

    Finished Alura course: Using Hooks

---
+++ src/hook/useProdutores.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import { carregaProdutores } from "../servicos/carregaDados";
+
+export default function useProdutores(){
+    const [titulo, setTitulo] = useState('');
+    const [lista, setLista] = useState([]);
+
+    useEffect(()=>{
+        const retorno = carregaProdutores();
+        retorno.lista.sort((a,b) => a.distancia - b.distancia);
+        console.log(retorno.lista);
+        setTitulo(retorno.titulo);
+        setLista(retorno.lista); 
+    },[])
+
+    return [titulo, lista];
+}
